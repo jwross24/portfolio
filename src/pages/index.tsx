150e04619ad6ab6369e40a8ea6120f9aca2344b1
jwross24 <jwross24@vt.edu>
2023-05-20 12:34:21 +0800

    add eslint-prettier and analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import UnderConstruction from '~/components/UnderConstruction';
+import UnderConstruction from "~/components/UnderConstruction";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Jonathan Ross | Portfolio</title>
-        <meta name="description" content="Jonathan Ross's Software Engineer Portfolio" />
+        <meta
+          name="description"
+          content="Jonathan Ross's Software Engineer Portfolio"
+        />
         <link rel="icon" href="/logo.svg" />
       </Head>
-      {process.env.NODE_ENV === 'development' ? <div>Dev Mode BAYBEE</div> : <UnderConstruction />}
+      {process.env.NODE_ENV === "development" ? (
+        <div>Dev Mode BAYBEE</div>
+      ) : (
+        <UnderConstruction />
+      )}
     </>
   );
 };
