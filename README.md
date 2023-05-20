07e82391125ecc28d78215e28e856462ae72750c
jwross24 <jwross24@vt.edu>
2023-05-20 12:21:43 +0800

    setup project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portfolio
+
+This is my software engineer portfolio project bootstrapped with `create-t3-app`.
+
+## Technologies
+
+- [Next.js](https://nextjs.org)
+- [Tailwind CSS](https://tailwindcss.com)
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import UnderConstruction from '~/components/UnderConstruction';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Jonathan Ross | Portfolio</title>
+        <meta name="description" content="Jonathan Ross's Software Engineer Portfolio" />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      {process.env.NODE_ENV === 'development' ? <div>Dev Mode BAYBEE</div> : <UnderConstruction />}
+    </>
+  );
+};
+
+export default Home;
